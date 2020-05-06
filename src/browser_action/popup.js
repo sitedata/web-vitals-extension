c7b73414105f1b8f4a86747351da587a7ca04003
Rick Viscomi <rviscomi@gmail.com>
2020-05-06 11:22:03 -0400

    connect API

--- src/browser_action/popup.js
+++ src/browser_action/popup.js
@@ -11,12 +11,12 @@
  limitations under the License.
 */
 
-const PSI_ENABLED = false;
-const API_KEY = '...';
-const API_URL = 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?';
+const FIELD_ENABLED = true;
+const API_KEY = 'AIzaSyAYyG49bJCCRiXqm1OmZitelBaZ4ZXB5Ro';
+const API_URL = 'https://chromeuxreport.googleapis.com/v1/records:queryRecord';
 const FE_URL = 'https://developers.google.com/speed/pagespeed/insights/';
 const encodedUrl = '';
-const resultsFetched = false;
+let resultsFetched = false;
 
 /**
  *
@@ -42,29 +42,46 @@ function hashCode(str) {
 
 /**
  *
- * Fetches API results from PSI API endpoint
+ * Fetches API results from CrUX API endpoint
  * @param {String} url
  * @returns
  */
 async function fetchAPIResults(url) {
-  if (PSI_ENABLED) {
-    if (resultsFetched) {
-      return;
-    }
-    const query = [
-      'url=url%3A' + url,
-      'key=' + API_KEY,
-    ].join('&');
-    const queryURL = API_URL + query;
-    try {
-      const response = await fetch(queryURL);
-      const json = await response.json();
-      createPSITemplate(json);
-    } catch (err) {
-      const el = document.getElementById('report');
-      el.innerHTML = `We were unable to process your request.`;
-    }
+  if (!FIELD_ENABLED) {
+    return;
   }
+  if (resultsFetched) {
+    return;
+  }
+
+  url = new URL(url);
+
+  const query = {
+    // TODO(rviscomi): Consider querying by URL instead.
+    'origin': url.origin,
+    'formFactor': 'desktop'
+  };
+
+  fetch(`${API_URL}?key=${API_KEY}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(query),
+  })
+  .then(response => response.json())
+  .then(response => {
+    if ('error' in response) {
+      return Promise.reject(response.error);
+    }
+    console.log('CrUX API response:', response);
+    createPSITemplate(response);
+  })
+  .catch(error => {
+    console.error('CrUX API error:', error);
+    const el = document.getElementById('report');
+    el.innerHTML = `We were unable to process your request.`;
+  });
 }
 
 /**
@@ -73,21 +90,66 @@ async function fetchAPIResults(url) {
  * @param {Object} result
  */
 function createPSITemplate(result) {
-  if (PSI_ENABLED) {
-    const experience = result.loadingExperience;
-    const metrics = experience.metrics;
-    const overall_category = experience.overall_category;
-    const fcp = metrics.FIRST_CONTENTFUL_PAINT_MS;
-    const fid = metrics.FIRST_INPUT_DELAY_MS;
-  
-    const fcp_template = buildDistributionTemplate(fcp, 'First Contentful Paint (FCP)');
-    const fid_template = buildDistributionTemplate(fid, 'First Input Delay (FID)');
-    const link_template = buildPSILink();
-    const tmpl = `<h1>Origin Performance (${overall_category})</h1> ${fcp_template} ${fid_template} ${link_template}`;
-    const el = document.getElementById('report');
-    el.innerHTML = tmpl;
-    // TODO: Implement per-tab/URL report caching scheme
-    resultsFetched = true;
+  if (!FIELD_ENABLED) {
+    return;
+  }
+
+  const metrics = result.record.metrics;
+  const lcp = metrics['largest_contentful_paint'];
+  const fid = metrics['first_input.delay'];
+  const cls = metrics['layout_instability.cumulative_layout_shift'];
+  const overall_category = getSummaryPerformanceLabel(lcp, fid, cls);
+
+  const lcp_template = buildDistributionTemplate(lcp, 'Largest Contentful Paint (LCP)');
+  const fid_template = buildDistributionTemplate(fid, 'First Input Delay (FID)');
+  const cls_template = buildDistributionTemplate(cls, 'Cumulative Layout Shift (CLS)');
+  const link_template = buildPSILink(result.record.key.origin);
+  const tmpl = `<h1>Origin Performance (${overall_category})</h1> ${lcp_template} ${fid_template} ${cls_template} ${link_template}`;
+  const el = document.getElementById('report');
+  el.innerHTML = tmpl;
+  // TODO: Implement per-tab/URL report caching scheme
+  resultsFetched = true;
+}
+
+/**
+ * Summarizes the performance of the origin/URL.
+ *
+ * - "Good" if all metrics are good.
+ * - "Poor" if any metric is poor.
+ * - "Needs Improvement" otherwise.
+ */
+function getSummaryPerformanceLabel(lcp, fid, cls) {
+  const labels = [
+    get75thPercentileLabel(lcp),
+    get75thPercentileLabel(fid),
+    get75thPercentileLabel(cls)
+  ];
+
+  if (labels.every(l => l == 'Good')) {
+    return 'Good';
+  }
+  if (labels.some(l => l == 'Poor')) {
+    return 'Poor';
+  }
+  return 'Needs Improvement';
+}
+
+/**
+ * Returns the good/NI/poor label in which the 75th percentile lives.
+ */
+function get75thPercentileLabel(metric) {
+  const labels = ['Good', 'Needs Improvement', 'Poor'];
+
+  metric.histogram.sort((a, b) => {
+    return a.start - b.start;
+  });
+
+  for (let i = 0, cdf = 0; i < 3; i++) {
+    const bin = metric.histogram[i];
+    cdf += bin.density;
+    if (cdf >= 0.75) {
+      return labels[i];
+    }
   }
 }
 
@@ -138,7 +200,7 @@ function buildLocalMetricsTemplate(metrics, tabLoadedInBackground) {
   </div>
 
     <div class="lh-footer lh-warning">
-      Mobile performance may be significantly slower. 
+      Mobile performance may be significantly slower.
       <a href="https://web.dev/load-fast-enough-for-pwa/" target="_blank">Learn more</a>
     </div>
   </div>
@@ -157,54 +219,40 @@ function renderLocalMetricsTemplate(metrics, tabLoadedInBackground) {
 }
 
 function buildDistributionTemplate(metric, label) {
+  function getDistributionBar(bin, data) {
+    return `<div
+      class="bar ${bin}"
+      title="${(data.density * 100).toFixed(2)}%"
+      style="flex-grow:${data.density * 100};">
+      ${Math.round(data.density * 100)}%
+    </div>`;
+  }
   return `<div class="field-data">
     <div class="metric-wrapper lh-column">
       <div class="lh-metric">
-        <div class="field-metric ${metric.category.toLowerCase()} lh-metric__innerwrap">
+        <div class="field-metric lh-metric__innerwrap">
           <span class="metric-description">${label}</span>
-          <div class="metric-value lh-metric__value">${formatDisplayValue(label, metric.percentile)}</div></div>
+        </div>
         <div class="metric-chart">
-          <div class="bar fast" style="flex-grow: 
-          ${Math.floor(metric.distributions[0].proportion * 100)};">
-          ${Math.floor(metric.distributions[0].proportion * 100)}%</div>
-          <div class="bar average" style="flex-grow: 
-          ${Math.floor(metric.distributions[1].proportion * 100)};">
-          ${Math.floor(metric.distributions[1].proportion * 100)}%</div>
-          <div class="bar slow" style="flex-grow: 
-          ${Math.floor(metric.distributions[2].proportion * 100)};">
-          ${Math.floor(metric.distributions[2].proportion * 100)}%</div>
-        </div></div>
+          ${getDistributionBar('fast', metric.histogram[0])}
+          ${getDistributionBar('average', metric.histogram[1])}
+          ${getDistributionBar('slow', metric.histogram[2])}
+        </div>
       </div>
-    </div> `;
+    </div>
+  </div>`;
 }
 
-function buildPSILink() {
+function buildPSILink(url) {
+  const encodedUrl = encodeURIComponent(url);
   return `<br><a href='${FE_URL}?url=${encodedUrl}' target='_blank'>
        View Report on PageSpeed Insights</a>`;
 }
 
-/**
- *
- * Format PSI API metric values
- * @param {String} metricName
- * @param {Number} metricValueMs
- * @returns
- */
-function formatDisplayValue(metricName, metricValueMs) {
-  if (metricValueMs === undefined) {
-    return '';
-  }
-  if (metricName === 'First Input Delay (FID)') {
-    return Number(metricValueMs.toFixed(0)) + ' ms';
-  } else {
-    return Number((metricValueMs / 1000).toFixed(1)) + ' s';
-  }
-};
-
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   const thisTab = tabs[0];
   // TODO: Re-enable PSI support once LCP, CLS land
-  if (PSI_ENABLED) {
+  if (FIELD_ENABLED) {
     fetchAPIResults(thisTab.url);
   }
 
@@ -212,7 +260,7 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   if (thisTab.url) {
     const key = hashCode(thisTab.url);
     const loadedInBackgroundKey = thisTab.id.toString()
-    
+
     let tabLoadedInBackground = false;
 
     chrome.storage.local.get(loadedInBackgroundKey, (result) => {
